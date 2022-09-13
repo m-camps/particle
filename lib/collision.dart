f7500f9dc4abfcf7af2723f0c66203d20a8c6bbb
Max <57141754+m-camps@users.noreply.github.com>
2022-09-13 19:41:02 +0200

    Made better class

--- lib/collision.dart
+++ lib/collision.dart
@@ -4,64 +4,98 @@ import 'package:flame/game.dart';
 import 'package:flame/input.dart';
 import 'package:flutter/material.dart' hide Image, Draggable;
 import 'dart:math';
+// ignore: unused_import
 import 'dart:developer' as developer;
 
 class CirclesExample extends FlameGame with HasCollisionDetection, TapDetector {
   @override
-  Color backgroundColor() => Color.fromARGB(255, 64, 63, 72);
+  Color backgroundColor() => const Color.fromARGB(255, 64, 63, 72);
 
   @override
   Future<void> onLoad() async {
     add(ScreenHitbox());
-    add(MyCircle(Vector2(size.x / 2, size.y / 2)));
+    add(FullCircle(Vector2(size.x / 2, size.y / 2), 400, 6));
   }
 }
 
-class MyCircle extends PositionComponent
+class FullCircle extends PositionComponent
     with HasGameRef<CirclesExample>, CollisionCallbacks {
+  final double radius;
+  final double segments;
+  final List<Color> colors = [
+    Colors.yellow,
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.purple,
+    Colors.cyan,
+  ];
+
   @override
-  MyCircle(pos) : super(position: pos);
+  FullCircle(pos, this.radius, this.segments) : super(position: pos);
+
+  @override
   Future<void> onLoad() async {
-    add(CircleArc(Vector2(size.x / 2, size.y / 2), 90, 90, 200, Colors.red));
-    add(CircleArc(Vector2(size.x / 2, size.y / 2), 180, 90, 200, Colors.green));
-    add(CircleArc(Vector2(size.x / 2, size.y / 2), 270, 90, 200, Colors.blue));
-    add(CircleArc(Vector2(size.x / 2, size.y / 2), 0, 90, 200, Colors.yellow));
+    final Vector2 centerPos = Vector2(size.x / 2, size.y / 2);
+    createCircle();
+  }
+
+  createCircle() {
+    final step = 360 / segments;
+    final Vector2 centerPos = Vector2(size.x / 2, size.y / 2);
+    int colorIndex = 0;
+    for (double i = 0; i < 360; i += step) {
+      add(CirclePart(
+          centerPos, i, step, radius, colors.elementAt(colorIndex), 40,
+          hitboxRes: 15, showHitbox: true));
+      colorIndex++;
+    }
   }
 
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-    angle += 2 * 0.01745329252;
+    //Rotation
+    angle += 1.5 * 0.01745329252;
   }
 }
 
-class CircleArc extends PositionComponent
+class CirclePart extends PositionComponent
     with HasGameRef<CirclesExample>, CollisionCallbacks {
   final double startAngle;
   final double sweepAngle;
   final double radius;
   final Color color;
+  final double stroke;
+  final bool showHitbox;
   late ShapeHitbox hitbox;
+  final double hitboxRes;
+  final hitboxPaint = Paint()
+    ..color = Colors.white
+    ..style = PaintingStyle.stroke;
 
-  CircleArc(
-      Vector2 pos, this.startAngle, this.sweepAngle, this.radius, this.color)
-      : super(position: pos);
+  CirclePart(
+    Vector2 pos,
+    this.startAngle,
+    this.sweepAngle,
+    this.radius,
+    this.color,
+    this.stroke, {
+    this.hitboxRes = 10,
+    this.showHitbox = false,
+  }) : super(position: pos);
 
   @override
   Future<void> onLoad() async {
-    final paint = Paint()
-      ..color = Colors.white
-      ..style = PaintingStyle.stroke;
-    hitbox = PolygonHitbox(calcVertices())
-      ..paint = paint
-      ..renderShape = false;
+    hitbox = PolygonHitbox(makeHitbox())
+      ..paint = hitboxPaint
+      ..renderShape = showHitbox;
     add(hitbox);
   }
 
   @override
   void render(Canvas canvas) {
     super.render(canvas);
-
     canvas.drawArc(
       Rect.fromCenter(
           center: const Offset(0, 0), width: radius, height: radius),
@@ -71,22 +105,21 @@ class CircleArc extends PositionComponent
       Paint()
         ..color = color
         ..style = PaintingStyle.stroke
-        ..strokeWidth = 20,
+        ..strokeWidth = stroke,
     );
   }
 
-  List<Vector2> calcVertices() {
+  List<Vector2> makeHitbox() {
     List<Vector2> inner = [];
     List<Vector2> outer = [];
 
-    for (double i = startAngle; i <= sweepAngle + startAngle; i += 10) {
-      inner.add(Vector2((radius / 2 - 10) * cos(i * 0.01745329252),
-          (radius / 2 - 10) * sin(i * 0.01745329252)));
+    for (double i = startAngle; i <= sweepAngle + startAngle; i += hitboxRes) {
+      inner.add(Vector2((radius / 2 - (stroke / 2)) * cos(i * 0.01745329252),
+          (radius / 2 - (stroke / 2)) * sin(i * 0.01745329252)));
+      outer.add(Vector2((radius / 2 + (stroke / 2)) * cos(i * 0.01745329252),
+          (radius / 2 + (stroke / 2)) * sin(i * 0.01745329252)));
     }
-    for (double i = startAngle; i <= sweepAngle + startAngle; i += 10) {
-      outer.add(Vector2((radius / 2 + 10) * cos(i * 0.01745329252),
-          (radius / 2 + 10) * sin(i * 0.01745329252)));
-    }
+    // for (double i = startAngle; i <= sweepAngle + startAngle; i += hitboxRes) {}
     outer = List.from(outer.reversed);
     return inner + outer;
   }
